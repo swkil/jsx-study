ec414978d31b53aae4ffee48312d125a9d96666f
swkil <0826monica@naver.com>
2025-07-30 10:22:35 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="card">
+        <img src="./avatar.webp" alt="avatar"/>
+        <h2>Alice</h2>
+        <h2>Programmer</h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
